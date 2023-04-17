0bd462615f321375c8a8a8e47325054d90c571e4
smagdela <smagdela@student.42.fr>
2023-04-17 15:51:00 +0200

    Chat implemented!

---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import '../styles/Chat.css'
+
+const Chat = () => {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className={`chat ${isExpanded ? 'expanded' : 'collapsed'}`}>
+      <div className="toggle-button" onClick={toggleExpand}>
+        {/* <image> "https://static-00.iconduck.com/assets.00/left-right-black-arrow-emoji-512x283-7vr1z2oc.png"</image> */}
+        {isExpanded ? '⬅' : '➡'} 
+      </div>
+      <input type="text" className="text-input" placeholder="Envoyer un message" />
+      <div className="content">
+        {"Simon : Bonjour"}
+      </div>
+    </div>
+  );
+};
+
+
+export default Chat;
