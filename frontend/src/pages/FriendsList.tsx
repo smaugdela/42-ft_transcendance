a0459f630f1283bdfe13a0e6924f8050a834bd23
Marine Sanjuan <marine@sanjuan.eu>
2023-04-17 22:40:33 +0200

    Logic in FriendsList page done. Everything works, missing the styling part

--- frontend/src/pages/FriendsList.tsx
+++ frontend/src/pages/FriendsList.tsx
@@ -1,22 +1,66 @@
-// import "../styles/FriendsList.css"
+import "../styles/FriendsList.css"
+import { IUser, users } from "../data";
 
 // faire un composant DisplayConnections
 // et selon les props qu'on passe, ça display tel
 // ou tel profil 
-export function FriendsList() {
+export function DisplayConnections( props: { profilesToDisplay : IUser[] }) {
+	
+	const displayProfiles = props.profilesToDisplay.map(profile => {
+		return <div key={profile.id}>
+					<img 
+						src={profile.avatar}
+						alt={profile.nickname}
+					/>
+					<h5>{profile.nickname}</h5>
+					<h5>🛇</h5>
+					<h5>✉</h5>
+		</div>
+	})
 	return (
 		<div>
+			{displayProfiles}
+		</div>
+	);
+};
+ 
+export function FriendsList() {
+
+	const loggedUser: IUser = users.filter( user => user.isLogged === true)[0];
+	
+	const allFriends: IUser[] = loggedUser.friendsList;
+	const activeFriends: IUser[] = allFriends.filter(friend => friend.isActive === true);
+	console.log(activeFriends);
+	
+	const blocked: IUser[] = loggedUser.blockList;
+	console.log(blocked);
+	
+	const pendingRequests: IUser[] = loggedUser.pendingList;
+	console.log(pendingRequests);
+	
+	return (
+		<div  id="friend-dashboard">
 			<h1>FRIENDS LIST</h1>
 			<input type="text" placeholder="Search.."/>
 			<br />
 			<br />
-			{/* <a href="">Active</a>
-			<span> / </span>
-			<a href="">All</a>
-			<span> / </span>
-			<a href="">Blocked</a>
-			<span> / </span>
-			<a href="">Pending</a> */}
+			<button>Active</button>
+			<DisplayConnections
+				profilesToDisplay={activeFriends}
+			/>
+			<button>All</button>
+			<DisplayConnections
+				profilesToDisplay={allFriends}
+			/>
+			
+			<button>Blocked</button>
+			<DisplayConnections
+				profilesToDisplay={blocked}
+			/>
+			<button>Pending</button>
+			<DisplayConnections
+				profilesToDisplay={pendingRequests}
+			/>
 		</div>
 	);
 };
