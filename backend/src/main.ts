1ca44cfde5890f5c124aedc605e237515efbe34a
smagdela <smagdela@student.42.fr>
2023-04-24 12:09:40 +0200

    Prisma working! Now work on nest nodules

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let port = Number(process.env.BACKEND_PORT);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+	if (Number.isNaN(port) == true)
+		port = 3001;
+
+  await app.listen(port);
+  console.log(`Backend started on port ${port}`);
 }
 bootstrap();
