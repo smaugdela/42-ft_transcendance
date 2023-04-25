0b34aea2f13db8a986e0cb9a1fae67a83b10a66d
smagdela <smagdela@student.42.fr>
2023-04-25 15:07:50 +0200

    Get user API by name and id

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+import * as fs from 'fs';
 
 let port = Number(process.env.BACKEND_PORT);
+const prisma = new PrismaClient();
+
+async function pushToDB_User (path: string)
+{
+	const jsonString = fs.readFileSync(path, 'utf-8');
+	const Data = JSON.parse(jsonString);
+
+	Data.forEach(async element => {
+		await prisma.user.create({
+			data: {
+				avatar: element.avatar,
+				nickname: element.nickname,
+				mailAddress: element.mailAddress,
+				coalition: element.coalition,
+		  },}).catch( (error) => console.log(error) );
+	});
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
+
+	pushToDB_User('../database/user_data.json'); // Use this only to load test data
+
+	console.log("Data loaded into db");
 
 	if (Number.isNaN(port) == true)
 		port = 3001;
 
-  await app.listen(port);
-  console.log(`Backend started on port ${port}`);
+	await app.listen(port);
+	console.log(`Backend started on port ${port}`);
 }
 bootstrap();
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,28 +1,51 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import UserDto from './user.dto';
 
 const prisma = new PrismaClient();
 
-@Controller('users')
+@Controller('/users')
 export class UsersController {
-	@Get()
-	async getUsers() {
-		return await prisma.user.findMany();
-	}
 
-	@Post()
-	async createUser(@Body() newUser: UserDto) {
-		console.log(newUser);
-		await prisma.user.create({
-			data: {
-				avatar: 		newUser.avatar,
-				nickname:		newUser.nickname,
-				mailAddress:	newUser.mailAddress,
-				coalition:		newUser.coalition,
-			},
-		}
-		);
-		console.log("New User Created!");
-	}
+  @Get()
+  async getUsers() {
+    return await prisma.user.findMany();
+  }
+
+  @Get(':userName')
+	async getUserByName(@Param('userName') userName: string) {
+	return await prisma.user.findUnique({
+		where: {
+			nickname: userName,
+		},
+	});
+  }
+
+  @Get('id/:userId')
+  async getUserById(@Param('userId') userId: number) {
+	return await prisma.user.findUnique({
+		where: {
+			id: Number(userId),
+		},
+	});
+  }
+
+
+  @Post()
+  async createUser(@Body() newUser: UserDto) {
+    console.log(newUser);
+    await prisma.user.create({
+      data: {
+        avatar: newUser.avatar,
+        nickname: newUser.nickname,
+        mailAddress: newUser.mailAddress,
+        coalition: newUser.coalition,
+      },
+    });
+    console.log('New User Created!');
+  }
+
+//   @Put()
+//   @Delete()
+
 }
