53feb9ebd4e2aa518907118583015b59c0c4d324
Marine SANJUAN <marine@sanjuan.eu>
2023-04-26 18:06:43 +0200

    userpage - CSS wip (main dashboard)

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -2,7 +2,7 @@ import "../styles/UserProfile.css"
 // import { IUser, users } from "../data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserPlus, faBan, faComment } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus, faBan, faComment, faDice, faHeart, faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faUserPlus);
 
@@ -15,7 +15,7 @@ export function UserProfile() {
 						<div id="hexagon-avatar"></div>
 						<div className="user-infos">
 							<div className="titles">
-								<h2>Marinozaure</h2>
+								<h2>marinozaure</h2>
 								<h1 id="status">ONLINE</h1>
 							</div>
 							
@@ -31,7 +31,15 @@ export function UserProfile() {
 					</article>
 					<hr />
 					<article>
-						<h1>Most importants stats</h1>
+						<FontAwesomeIcon icon={faDice} />
+						<h2>12</h2>
+						<h5>Total Matches</h5>
+						<FontAwesomeIcon icon={faTrophy} />
+						<h2>9</h2>
+						<h5>Victories</h5>
+						<FontAwesomeIcon icon={faHeart} />
+						<h2>3</h2>
+						<h5>Friends</h5>
 					</article>
 					<hr />
 					<article>
@@ -45,13 +53,13 @@ export function UserProfile() {
 			</section>
 			<aside>
 				<h1>Match History (last 3)</h1>
-				<div>
+				<div className="match-card">
 					<h5>24 avril 2023 22h48 - 6''03</h5>
 					<h4 className="match-outcome defeat"> DEFEAT !</h4>
 					<div className="match-detail">
 						<div className="opponent">
 							<img src="/assets/jinx.png" alt="av" />
-							<h4>Marinozaure</h4>
+							<h4>marine</h4>
 						</div>
 						<div>
 							<h2>1 - 2</h2>
@@ -62,13 +70,13 @@ export function UserProfile() {
 						</div>
 					</div>
 				</div>
-				<div>
+				<div className="match-card">
 				<h5>22 avril 2023 19h36 - 3''22</h5>
 				<h4 className="match-outcome victory"> VICTORY !</h4>
 					<div className="match-detail">
 						<div className="opponent">
 							<img src="/assets/jinx.png" alt="av" />
-							<h4>Marinozaure</h4>
+							<h4>marine</h4>
 						</div>
 						<div>
 							<h2>3 - 2</h2>
@@ -79,13 +87,13 @@ export function UserProfile() {
 						</div>
 					</div>
 				</div>
-				<div>
+				<div className="match-card">
 				<h5>21 avril 2023 14h26 - 2''14</h5>
 				<h4 className="match-outcome ace"> ACE !</h4>
 					<div className="match-detail">
 						<div className="opponent">
 							<img src="/assets/jinx.png" alt="av" />
-							<h4>Marinozaure</h4>
+							<h4>marine</h4>
 						</div>
 						<div>
 							<h2>2 - 0</h2>
