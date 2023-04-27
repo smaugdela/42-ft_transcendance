0b5e52cdcd77cd43889aff50bb51c8cdcace9ce4
fahima8897 <fboumellil@gmail.com>
2023-04-27 16:51:05 +0200

    about us page ongoing

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -13,25 +13,25 @@ const teamMembers: Member[] = [
     name: 'Simon',
     position: 'COO',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/200',
   },
   {
     name: 'Anna',
     position: 'COO',
     description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/200',
   },
   {
     name: 'Marine',
     position: 'CTO',
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image: 'https://via.placeholder.com/150',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+    image: 'https://via.placeholder.com/200',
   },
   {
     name: 'Fahima',
     position: 'CTO',
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image: 'https://via.placeholder.com/150',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+    image: 'https://via.placeholder.com/200',
   },
 ];
 
@@ -40,11 +40,10 @@ function AboutUs() {
     <div className='background_aboutus'>
       <section className='aboutus'>
         <h1>Who are we ?</h1>
-        <p>We are the super team that created the best game online ever </p>
       </section>
       <section className='container'>
        {teamMembers.map((member, index) => (
-      <div key={index}>
+      <div  className='container_staff' key={index}>
         <img src={member.image} alt={member.name} />
         <h2>{member.name}</h2>
         <p>{member.position}</p>
@@ -52,6 +51,9 @@ function AboutUs() {
       </div>
     ))}
     </section>
+      <section>
+        <p>We are the super team that created the best game online called Pong Game </p>
+      </section>
   </div>
   );
 }
