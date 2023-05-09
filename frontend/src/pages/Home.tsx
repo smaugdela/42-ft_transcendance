f7b95c71759275fbba7668c7e30dab0c95e2b995
ajearuth <ajearuth@student.42.fr>
2023-05-09 18:41:26 +0200

    boutton glitch

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,8 @@ import'../styles/Home.css';
 export default function Home() {
 	return (
         <div id="play-screen">
-          <button className='button1' >PRESS TO PLAY</button>
-        </div>
+          <button className='glitch' data-text="PRESS TO PLAY">PRESS TO PLAY
+		  </button>
+	    </div>
 	);
 }
