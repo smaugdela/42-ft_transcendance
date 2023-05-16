19edf35dd07085242bd66b1043d322ef872af573
fahima8897 <fboumellil@gmail.com>
2023-05-16 17:10:48 +0200

    fixing front - Chat

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import '../styles/Chat.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCat, faComment, faAnglesLeft, faAnglesRight} from "@fortawesome/free-solid-svg-icons";
+import Tab_channels from './Tab_channels';
 
+interface Tab {
+  label: string;
+  content: JSX.Element;
+}
 
 
 
@@ -13,22 +18,40 @@ const Chat = () => {
     setIsExpanded(!isExpanded);
   };
 
-  // const SidebarData = [
-  //   {
-  //     image: <FontAwesomeIcon className='item_image' icon={faCat} />,
-  //   }
-  // ];
+
+
+  const [activeTab, setActiveTab] = useState(0);
+  const tabs: Tab[] = [
+    { label: 'Channels', content: <div><Tab_channels/></div> },
+    { label: 'Chat', content: <div>Content 1</div> },
+    { label: 'Settings', content: <div>Content 3</div> },
+  ];
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
+
+
   return (
     <div className={`chat ${isExpanded ?  'collapsed': 'expanded'}`}>
       <div className="toggle-button" onClick={toggleExpand}>
-      {/* <div> </div> */}
         {isExpanded ? <FontAwesomeIcon icon={faAnglesLeft}/> : <FontAwesomeIcon icon={faAnglesRight}/>} 
       </div>
       <input type="text" className="text-input" placeholder="Envoyer un message" />
       <div className="content">
-        {"Simon : Bonjour"}
+        {/* {"Simon : Bonjour"} */}
+        {tabs.map((tab, index) => (
+          <div
+            key={index}
+            className={`tab ${index === activeTab ? 'active' : ''}`}
+            onClick={() => handleTabClick(index)}>
+            <button className='chat_button'>{tab.label}</button>
+          </div>
+        ))}
       </div>
-    </div>
+      <div className="tab-content">{tabs[activeTab].content}</div>
+      </div>
+    // </div>
   );
 };
 
---
+++ frontend/src/components/Tab_channels.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/Tab_channels.css';
+
+function Tab_channels() {
+  return (
+    <div className='channels_page'>
+      <ul >
+        <h1>Joined channels</h1>
+        <li>Pro_Gamers</li>
+        <li>Marvel_fans</li>
+        <li>...</li>
+        <br />
+      </ul>
+      <ul>
+        <h1>Channels created</h1>
+        <li>Code_war</li>
+        <li>LifeStyle</li>
+        <li>...</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Tab_channels
