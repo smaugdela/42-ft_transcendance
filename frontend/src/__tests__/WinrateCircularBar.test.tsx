e7dfb0a18cda1c7074aa66aafbad213a6a0e8345
Marine SANJUAN <marine@sanjuan.eu>
2023-05-18 11:40:53 +0200

    ⏪ [FRONT] [TESTS] Revert changes, testing setup not working

--- frontend/src/__tests__/WinrateCircularBar.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe } from "node:test";
-
-describe("<WinrateCircularBar />", () => {
-	test("Props should be a number <= 100"), () => {
-		const props = 100;
-		expect(props).toBe(100);
-	}
-});
--- frontend/src/globals.d.ts
+++
@@ -1,3 +0,0 @@
-// To ensure the additional Jest matchers are available for all test files, 
-// create a src/globals.d.ts and import the matchers:
-import "@testing-library/jest-dom/extend-expect";
