e0170b4ebdb99fcc97ee9b63d52631298fc0cb1b
ajearuth <ajearuth@student.42.fr>
2023-05-23 15:49:04 +0200

    mode solo and multi buttons

---
+++ frontend/src/pages/GamePage.tsx
@@ -0,0 +1,13 @@
+import '../App.css';
+import'../styles/GamePage.css';
+
+export default function GamePage() {
+	return (
+        <div id="play-screen">
+          <button className='button1' data-text="MODE SOLO">MODE SOLO
+		  </button>
+		  <button className='button2' data-text="MODE MULTI">MODE MULTI
+		  </button>
+	    </div>
+	);
+}
