c8de1ecb938ce9f3e3f95363ee721f78777f803b
smagdela <smagdela@student.42.fr>
2023-05-23 13:29:51 +0200

    WIP implementing auth guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
+	imports: [JwtModule.register({
+		global: true,
+		secret: jwtConstants.secret,
+		signOptions: { expiresIn: '60' }, // In seconds by default
+	})],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- backend/src/auth/guards/auth.guard.ts
+++ backend/src/auth/guards/auth.guard.ts
@@ -1,29 +1,61 @@
-import { CanActivate, Injectable, ExecutionContext, ForbiddenException } from "@nestjs/common";
+import { CanActivate, Injectable, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { PrismaClient } from "@prisma/client";
+import { Observable } from "rxjs";
+import { jwtConstants } from "../constants";
+import { JwtService } from "@nestjs/jwt";
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-	async canActivate(context: ExecutionContext): Promise<boolean>
-	{
+	constructor(private jwtService: JwtService);
+
+	async canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
 		const request = context.switchToHttp().getRequest();
+		const jwt = this.extractTokenFromHeader(request);
 
-		const id: number = request.cookies.id;
-		const accessToken: string = request.cookies.accessToken;
-
-		const user = await prisma.user.findUnique({
-			where: {
-				id: +id,
-			}
-		});
-
-		if (!user)
-			throw new ForbiddenException('Unauthorized access');
-		else if (user.accessToken !== accessToken)
-			throw new ForbiddenException('Invalid Access Token.')
-
+		if (!jwt)
+		{
+			throw new UnauthorizedException('No access token.');
+		}
+		try {
+			const payload = await this.jwtService.verifyAsync(jwt, {
+				secret: jwtConstants.secret,
+			});
+			request['user'] = payload;
+		} catch {
+			throw new UnauthorizedException('Bad access token.');
+		}
 		return true;
 	}
+
+	private extractTokenFromHeader(request: Request): string | undefined {
+		const [type, token] = request.headers.authorization?.split(' ') ?? [];
+		return type === 'Bearer' ? token : undefined;
+	}
+
+	// This next canActivate() function is used in conjunction with cookies and an access token.
+
+	// async canActivate(context: ExecutionContext): Promise<boolean>
+	// {
+	// 	const request = context.switchToHttp().getRequest();
+
+	// 	const id: number = request.cookies.id;
+	// 	const accessToken: string = request.cookies.accessToken;
+
+	// 	const user = await prisma.user.findUnique({
+	// 		where: {
+	// 			id: +id,
+	// 		}
+	// 	});
+
+	// 	if (!user)
+	// 		throw new ForbiddenException('Unauthorized access');
+	// 	else if (user.accessToken !== accessToken)
+	// 		throw new ForbiddenException('Invalid Access Token.')
+
+	// 	return true;
+	// }
 }
