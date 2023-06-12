7e9555a85502b4e94bb7cd4af08dd235ddc95963
Marine Sanjuan <marine@sanjuan.eu>
2023-06-12 15:19:09 +0200

    :construction: [BACK] [CHAT] wip on websockets setup

---
+++ backend/src/chat/chat.gateway.ts
@@ -0,0 +1,15 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway(3001, { namespace: 'chat'}) // url is : ws://localhost:3001/chat
+export class ChatGateway {
+	
+	@WebSocketServer()
+	server;
+
+	// extract out the message from the data payload
+	@SubscribeMessage('message')
+	handleMessage(@MessageBody() message: string): void {
+		this.server.emit('message', message); // 'message' is the name of the event we're emitting
+	}
+	// broadcast an incoming message to all listeners of this gateway; with namespace chat
+}
