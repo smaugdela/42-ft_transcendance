9e5376686b72b96250c564be7c425effb9a35dab
Marine SANJUAN <marine@sanjuan.eu>
2023-06-23 13:28:13 +0200

    erased create-user dto bc not meaningful

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,8 +1,36 @@
-import { PartialType } from '@nestjs/mapped-types';
-import CreateUserDto from './create-user.dto';
-import { IsBoolean, IsNumber, IsOptional, IsPositive } from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional, IsPositive, IsString, IsNotEmpty } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
+	@IsNotEmpty()
+	@IsString()
+	nickname:		string;
+
+	@IsString()
+	@IsNotEmpty()
+	accessToken:	string;
+
+	@IsString()
+	@IsNotEmpty()
+	coalition:		string;
+
+	@IsOptional()
+	@IsString()
+	@IsNotEmpty()
+	avatar:			string;
+
+	@IsOptional()
+	@IsString()
+	@IsNotEmpty()
+	email:	string;
+
+	@IsOptional()
+	@IsString()
+	bio:	string;
+
+	@IsNotEmpty()
+	@IsString()
+	password:		string;
+	
 	@IsOptional()
 	@IsNumber()
 	@IsPositive()
