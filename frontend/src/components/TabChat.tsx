f5c2cc46ecba3c2508ae3d628a638c2592b9f18b
smagdela <smagdela@student.42.fr>
2023-06-26 14:01:19 +0200

    WIP implementing websockets. Trying to connect to backend server using any kind of testing software, next one is Postman

--- frontend/src/components/TabChat.tsx
+++ frontend/src/components/TabChat.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Tab_Chat.css';
+import io from 'socket.io-client';
 
 function TabChat() {
-  return (
-    <div>
-       <input type="text" className="text-input" placeholder="Envoyer un message" />
-    </div>
-  )
+
+	const [messages, setMessages] = useState<string[]>([]);
+	const [inputValue, setInputValue] = useState('');
+	const [socket, setSocket] = useState<any>(null); // Set any as the type for socket
+  
+	const sendMessage = (message: string) => {
+	console.log("Sending Message");
+	  if (socket) {
+		console.log("socket defined");
+		socket.emit('chatMessage', message);
+		setInputValue('');
+	  }
+	};
+  
+	useEffect(() => {
+	  const newSocket = io('http://localhost:3001');
+	  setSocket(newSocket);
+  
+	  newSocket.on('chatMessage', (message: string) => {
+		console.log("Message received");
+		setMessages((prevMessages) => [...prevMessages, message]);
+	  });
+  
+	  return () => {
+		newSocket.disconnect();
+	  };
+	}, []);
+  
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+	  setInputValue(event.target.value);
+	};
+  
+	const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+	  event.preventDefault();
+	  if (inputValue.trim() !== '') {
+		sendMessage(inputValue);
+	  }
+	};
+
+	return (
+		<div>
+		  <ul>
+			{messages.map((message, index) => (
+			  <li key={index}>{message}</li>
+			))}
+		  </ul>
+		  <form onSubmit={handleFormSubmit}>
+			<input
+			  type="text"
+			  value={inputValue}
+			  onChange={handleInputChange}
+			  placeholder="Type your message..."
+			/>
+			<button type="submit">Send</button>
+		  </form>
+		</div>
+	  );
+
+//   return (
+//     <div>
+//        <input type="text" className="text-input" placeholder="Envoyer un message" />
+//     </div>
+//   )
 }
 
 export default TabChat
