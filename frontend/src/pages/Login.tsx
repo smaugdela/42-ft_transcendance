1ddf064a7cf2e315c8946b38df2fac70b7a7772a
Marine SANJUAN <marine@sanjuan.eu>
2023-06-26 18:20:56 +0200

    formatted page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,47 +5,48 @@ import { signUp } from "../api/APIHandler";
 
 export default function Login() {
 
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
+	const [nickname, setNickname] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleNickname = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNickname(event.target.value);
-  }
+	const handleNickname = (event: React.ChangeEvent<HTMLInputElement>) => {
+	setNickname(event.target.value);
+	}
 
-  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  }
+	const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+	setPassword(event.target.value);
+	}
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-   event.preventDefault();
-  
-   const response = signUp(nickname, password).then((response) => {console.log(response)});
-   console.log(response);
-  }
+	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+	event.preventDefault();
+	console.log("je veux m'inscrire");
 
-  return (
-    <div className="Login">
-      <div className="background">
-      </div>
-      <form>
-        {/* <h3>Login Here</h3> */}
+	const response = signUp(nickname, password).then((response) => {console.log(response)});
+	console.log(response);
+	}
 
-        <label htmlFor="username">Username</label>
-        <input onChange={handleNickname} type="text" placeholder="Email or Phone" id="username" />
+	return (
+	<div className="Login">
+		<div className="background">
+		</div>
+		<form>
+		{/* <h3>Login Here</h3> */}
 
-        <label htmlFor="password">Password</label>
-        <input onChange={handlePassword} type="password" placeholder="Password" id="password" />
+		<label htmlFor="username">Username</label>
+		<input onChange={handleNickname} type="text" placeholder="Email or Phone" id="username" />
 
-        <button onClick={handleClick} id="login-btn">Log In</button>
-        <div className="social">
-          <div className="go">
-            <i className="fab fa-google"></i> Log with 42
-          </div>
-          <div className="fb">
-            <i className="fab fa-facebook"></i> Sign up
-          </div>
-        </div>
-      </form>
-    </div>
+		<label htmlFor="password">Password</label>
+		<input onChange={handlePassword} type="password" placeholder="Password" id="password" />
+
+		<button onClick={handleClick} id="login-btn">Log In</button>
+		<div className="social">
+			<div className="go">
+				<i className="fab fa-google"></i> Log with 42
+			</div>
+			<div className="fb">
+				<i className="fab fa-facebook"></i> Sign up
+			</div>
+		</div>
+		</form>
+	</div>
   );
 }
