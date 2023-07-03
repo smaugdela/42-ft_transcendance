5d598810a841a68722895acf21683232633f70e3
smagdela <smagdela@student.42.fr>
2023-07-03 18:53:00 +0200

    Fixed a good chunk of problems and quality of life stuffs: ability to create an account with 42 API when another user has the same login, correct redirection when created, fixed bugs, identified user on socket, etc...

---
+++ backend/src/sockets/middleware/username.middleware.ts
@@ -0,0 +1,68 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import * as cookieParser from 'cookie-parser';
+import { jwtConstants } from 'src/auth/constants';
+import { PrismaClient } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+
+const prisma = new PrismaClient();
+
+export function usernameMiddleware(jwtService: JwtService) {
+	return async (client: Socket, next: (err?: any) => void) => {
+
+		const cookie: string = client.handshake.headers.cookie;
+		if (!cookie)
+			return next(new UnauthorizedException('No cookies, no access.'));
+
+		// We extract the jwt from the cookies
+		const jwt: string = cookie.split('; ').find((cookie: string) => cookie.startsWith('jwt')).split('=')[1];
+		if (!jwt)
+			return next(new UnauthorizedException('No token found.'));
+
+		// We unsign the jwt cookie to make it readable
+		const unsignedJwt = cookieParser.signedCookie(decodeURIComponent(jwt), process.env.COOKIE_SECRET);
+		if (!unsignedJwt)
+			return next(new UnauthorizedException('Bad cookie signature.'));
+
+		// We check the token's validity
+		try {
+			const payload = await jwtService.verifyAsync(unsignedJwt, {
+				secret: jwtConstants.secret,
+			});
+
+			if (!payload)
+				return next(new UnauthorizedException('Bad token payload.'));
+			const userId = payload.sub;
+
+			// We check if the user exists
+			const user = await prisma.user.findUniqueOrThrow({
+				where: {
+					id: userId,
+				},
+			});
+			if (!user)
+				return next(new UnauthorizedException('No user associated with this token.'));
+
+			// We tell that the user is active
+			if (user.isActive === false) {
+				await prisma.user.update({
+					where: {
+						id: userId,
+					},
+					data: {
+						isActive: true,
+					},
+				});
+			}
+
+			// We add the username to the socket instance for identification
+			client.data.username = user.nickname;
+			client.data.userId = user.id;
+
+		} catch (error) {
+			return next(new UnauthorizedException('Bad token.'));
+		}
+
+		next();
+	}
+}
