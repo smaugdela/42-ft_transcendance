2c560068f4994ee7be0d2ad0872f16fe498c972d
smagdela <smagdela@student.42.fr>
2023-08-01 15:53:34 +0200

    Push for merge to fix docker compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // 	const prisma = new PrismaClient();
 // 	const jsonString = fs.readFileSync(path, 'utf-8');
 // 	const Data = JSON.parse(jsonString);
-
 // 	Data.forEach(async element => {
 // 		await prisma.user.create({
 // 			data: {
@@ -29,7 +28,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 
-	console.log("Backend Port: " + process.env.BACKEND_PORT);
+	// console.log("Backend Port: " + process.env.BACKEND_PORT);
 
 	const port = Number(process.env.BACKEND_PORT);
 	if (isNaN(port)) {
@@ -63,11 +62,13 @@ async function bootstrap() {
 
 	app.use(cookieParser(process.env.COOKIE_SECRET));
 
-	app.enableCors({
-		credentials: true,
-		// origin: process.env.FRONTEND_URL,
-		origin: "https://" + process.env.FRONTEND_HOST,
-	});
+	let corsConfig;
+	if (process.env.DOCKER && process.env.DOCKER === "true")
+		corsConfig = { origin: "http://" + process.env.FRONTEND_HOST, credentials: true };
+	else
+		corsConfig = { origin: process.env.FRONTEND_URL, credentials: true };
+
+	app.enableCors(corsConfig);
 
 	await app.listen(port);
 
