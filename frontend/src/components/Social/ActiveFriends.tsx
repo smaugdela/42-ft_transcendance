1536a6b9e051f5755ab26022022349d4d188f339
fboumellil@gmail.com <fboumellil@gmail.com>
2023-08-02 18:40:49 +0200

    fixing the front of social

--- frontend/src/components/Social/ActiveFriends.tsx
+++ frontend/src/components/Social/ActiveFriends.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from 'react';
 import "../../styles/Social.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComment} from '@fortawesome/free-solid-svg-icons';
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { IUser } from "../../api/types";
 import MessageUserBtn from '../Profile/MessageUserBtn';
 import { fetchMe } from "../../api/APIHandler";
 
 export function ActiveFriends( props: { profilesToDisplay : IUser[]}) {
-        // const queryClient = useQueryClient();
-        // const removefriend = useMutation({ 
-        //     mutationFn: (id: number) => removeFriend(id),
-        //     onSuccess: () => {
-        //         queryClient.invalidateQueries(['user']);	
-        //     }
-        // });
-        // const blockuser = useMutation({ 
-        //     mutationFn: (nickname: string) => blockUser(nickname),
-        //     onSuccess: () => {
-        //         queryClient.invalidateQueries(['user']);	
-        //     }
-        // });
-        // useEffect(() => {
-        //     if (props.userIsSuccess) {
-        //       queryClient.invalidateQueries(['user']); // Refetch the user data if the userIsSuccess prop changes
-        //     }
-        //   }, [props.userIsSuccess]);
-    
-        // const handleremoveFriend = (id: number) => {
-        //     removefriend.mutate(id);
-        // }
-        // const handleblockuser = (nickname: string) => {
-        //     blockuser.mutate(nickname);
-        // }
-
         const queryClient = useQueryClient();
         const { data: loggedUser, error, isLoading, isSuccess } = useQuery({ queryKey: ['user'], queryFn: fetchMe});
         
@@ -46,8 +17,6 @@ export function ActiveFriends( props: { profilesToDisplay : IUser[]}) {
         
         const loggedInUser: string = loggedUser.nickname;;
 	
-	
-        
         const displayProfiles = props.profilesToDisplay.map(profile => {
             return <div key={profile.id} className="profile">
                         <div className="img-container">
@@ -58,10 +27,7 @@ export function ActiveFriends( props: { profilesToDisplay : IUser[]}) {
                         </div>
                         <div className="profile_infos">
                             <h5>{profile.nickname}</h5>
-                            {/* <div><FontAwesomeIcon icon={faComment} /></div> */}
                             <MessageUserBtn loggedInUser={loggedInUser} userToContact={profile} />
-
-                            {/* <div><FontAwesomeIcon icon={faBan} onClick={() =>handleblockuser(profile.nickname)}/></div>			 */}
                         </div>
             </div>
         })
